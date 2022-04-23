11b426ff2b8e3f837c22a3806d2faf819008dacb
Thiago099 <tecnologiasthiago@gmail.com>
2022-04-23 14:18:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Vue electron</h1>
+</template>
+
+<script lang="ts" setup>
+// import { ref } from 'vue';
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
